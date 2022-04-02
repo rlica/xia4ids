e91a16150b462bad68886d17cf41a4cac67e41ac
Razvan Lica <razvan.lica@cern.ch>
2022-04-02 06:57:14 +0200

    Redefined calibrate function, now a double, adds random number to ADC channel
    This removes binning issues when going from ADC channels, to energies. This issue is seen when binning of energy histogram is mismatched relative to ADC channels - can be seen as either spikes or gaps in certain histogram bins
    
    Co-Authored-By: jgcubiss <20517912+jgcubiss@users.noreply.github.com>
    Co-Authored-By: Chris Page <97673392+caapage@users.noreply.github.com>

--- inc/calibrate.hh
+++ inc/calibrate.hh
@@ -1,32 +1,21 @@
-
 //Calibrating each signal, using random number generator
 //If the signal has no calibration defined, the energy will be the same
 
-int calibrate(int module, int channel, int energy) {
-  
-  
+double calibrate(int module, int channel, int energy) {
+	
+	// If no calibration for channel, return ADC energy
     if (have_cal == 0 || calib[module][channel][0] == 0) return energy;
 	  
-    int i;
-    
-    //srand(time(NULL)); this is very bad if doing it more than once per second, therefore we only do it once, in the main()
+	// Generate random number between 0 and 1, and add to ADC channel
+	// to remove binning issues
+	static std::default_random_engine generator;
+	std::uniform_real_distribution<double> distribution(-0.5,0.5);
+	double engPlusRand = energy + distribution(generator);
 	
-	double random_number, prob;
-	double d_energy = 0;
-	int    i_energy = 0;
-	
-	for (i=0; i<MAX_CAL; i++) d_energy += calib[module][channel][i] * pow(energy, i);
-	 
-	i_energy = d_energy;
-	
-    random_number =  (double)rand() / ((double)RAND_MAX + 1.0);
-    
-	prob = (d_energy - i_energy);
-
-  	if (random_number < prob) i_energy += 1;
-	
-	return i_energy;
-
+	// Apply calibration and return energy
+	double d_energy = 0.;
+	for(int i=0; i<MAX_CAL; i++) d_energy += calib[module][channel][i] * pow(engPlusRand, i);
+	return d_energy;
 }
 
 
